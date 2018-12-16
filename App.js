293552990db6988e454c28f7cc687ff9628b1a29
Kumar Rangarajan <kumar@slanglabs.in>
2018-12-16 23:51:21 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,105 @@
+/**
+ * Sample React Native App to demonstrate Slang integration
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {NativeEventEmitter, NativeModules, Platform, StyleSheet, Text, View} from 'react-native';
+
+const { SlangBuddy } = NativeModules;
+
+var slangEmitter;
+
+type Props = {};
+export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      intent: "unknown",
+      unresolvedEntity: "unknown",
+      resolvedEntity: "unknown",
+    };
+  }
+
+  componentDidMount() {
+    slangEmitter = new NativeEventEmitter(SlangBuddy);
+    // Initialize Slang. This is using the same AirtelMock credentials as in
+    // https://github.com/SlangLabs/airtelmock
+    SlangBuddy.initialize(
+      "968fd6e6b1a347beb7b21b966f6b3c9d", 
+      "c80525dd5fa146d6a3a1aba91fc5d6b9",
+      () => {
+        console.log("initialized slang ");
+        console.log("registering actions ");
+        this.registerHandlers();
+      },
+      (reason) => {
+        console.log("error initializing slang - " + reason);
+      }
+    );
+  }
+
+  registerHandlers() {
+    slangEmitter.addListener('roaming', (e: Event) => {
+      console.log(e);
+      continueSession = true;
+      this.setState({intent: null, intent: e.intent.name});
+      // handle event.
+      switch (e.eventType) {
+        case "unresolvedEntity":
+          this.setState({unresolvedEntity: e.unresolvedEntity.name});
+          if (e.unresolvedEntity.name === "country" && e.entities["region"].value === "domestic") {
+            console.log("setting entity country");
+            SlangBuddy.setEntity("country", "india");
+          }
+          break;
+
+        case "action":
+          break;
+      }
+
+      if (continueSession) {
+        console.log("continuing session...");
+        SlangBuddy.continueSession();
+      } else {
+        console.log("failing session...");
+        SlangBuddy.failSession(); 
+      }
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to Slang React Native Demo!</Text>
+        <Text style={styles.instructions}>To get started, click on the speaker icon and start talking</Text>
+        <Text/>
+        <Text style={styles.instructions}>Current intent: {this.state.intent}</Text>
+        <Text style={styles.instructions}>Current unresolved entity: {this.state.unresolvedEntity}</Text>
+        <Text style={styles.instructions}>Current resolved entity: {this.state.resolvedEntity}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ README.md
@@ -0,0 +1,15 @@
+# Slang React native integration with Sample app 
+
+# Add dependency to Slang
+
+$ npm install --save git+ssh://git@github.com/slanglabs/react-native-slang.git
+
+# Ensure that all other dependencies are installed
+
+$ npm install
+
+# Run the android version
+
+$ react-native run-android
+
+NOTE: Currently only the Android version is supported
